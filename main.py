7eac035fdc7610ad6e37d8231c7ebde1c4bfd926
sysmed <sake3b@gmail.com>
2022-08-25 18:53:41 -0400

    polish order func

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import base64
 import hashlib
 import hmac
 import json
+import random
 
 import requests
 import decimal
@@ -12,7 +13,9 @@ import time
 URL = 'https://api.kucoin.com'
 KUCOIN_ORDERS = '/api/v1/orders'
 
-
+API_KEY = "63066e7e650b2d00012fa331"
+API_SECRET = "d2e0887c-456d-417f-ba5d-d1157ae140cd"
+API_PASSPHRASE = "1234567"
 
 
 def get_currency_moving_average(currency_code, number_of_months):
@@ -31,13 +34,14 @@ def get_currency_moving_average(currency_code, number_of_months):
 
     return 0
 
-def make_limit_order(currency_code):
-    print('made order')
-
-    payload = {'clientOid': '343AS2ew_', 'side': 'buy', 'symbol': 'BTC-USDT', 'type': 'market', 'funds': '5'}
+def make_limit_order(base_currency_symbol, quote_currency_symbol, quote_amount):
+    order_id = random.getrandbits(128)
+    payload = {'clientOid': order_id, 'side': 'buy',
+               'symbol': f'{base_currency_symbol}-{quote_currency_symbol}'
+               , 'type': 'market', 'funds': f'{quote_amount}'}
     payload_json = json.dumps(payload)
-    headers = create_header(payload_json)
-    result = requests.request('post', URL + KUCOIN_ORDERS, data=payload_json, headers=headers)
+    headers = create_header('POST', API_KEY, API_SECRET, API_PASSPHRASE, payload_json)
+    result = requests.request('POST', URL + KUCOIN_ORDERS, data=payload_json, headers=headers)
     print(result.json())
 def get_currency_history(currency_code):
     ticker = requests.get(URL + '/api/v1/market/histories?symbol=' + currency_code + '-USDT').json()
@@ -48,12 +52,12 @@ def get_current_currency_price(currency_code):
     ticker = requests.get(URL + '/api/v1/prices', params=payload).json()
     current_price = ticker['data'][currency_code]
     return decimal.Decimal(current_price)
-def create_header(data):
+def create_header(method, api_key, api_secret, api_passphrase, data):
     time_now = int(time.time() * 1000)
-    api_key = "63066e7e650b2d00012fa331"
-    api_secret = "d2e0887c-456d-417f-ba5d-d1157ae140cd"
-    api_passphrase = "1234567"
-    str_to_sign = str(time_now) + 'POST' + KUCOIN_ORDERS + data
+    api_key = api_key
+    api_secret = api_secret
+    api_passphrase = api_passphrase
+    str_to_sign = str(time_now) + method + KUCOIN_ORDERS + data
     signature = base64.b64encode(
         hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
     passphrase = base64.b64encode(
@@ -67,30 +71,10 @@ def create_header(data):
         "Content-Type": "application/json"  # specifying content type or using json=data in request
     }
     return headers
-def test_func():
-    api_key = "63066e7e650b2d00012fa331"
-    api_secret = "d2e0887c-456d-417f-ba5d-d1157ae140cd"
-    api_passphrase = "1234567"
-    url = URL + '/api/v1/accounts'
-    now = int(time.time() * 1000)
-    str_to_sign = str(now) + 'GET' + '/api/v1/accounts'
-    signature = base64.b64encode(
-        hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
-    passphrase = base64.b64encode(
-        hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), hashlib.sha256).digest())
-    headers = {
-        "KC-API-SIGN": signature,
-        "KC-API-TIMESTAMP": str(now),
-        "KC-API-KEY": api_key,
-        "KC-API-PASSPHRASE": passphrase,
-        "KC-API-KEY-VERSION": "2"
-    }
-    response = requests.request('get', url, headers=headers)
-    print(response.status_code)
-    print(response.json())
+
 if __name__ == "__main__":
     #print(get_currency_moving_average('BTC', 4))
     #print(get_currency_moving_average('BTC', 2))
 
-    make_limit_order('BTC')
+    make_limit_order('BTC', 'USDT', 2)
     #test_func()
